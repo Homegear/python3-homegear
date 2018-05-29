803d39be97783887cd77832da42017af88376b00
Sathya Laufer <sathya@laufers.net>
2018-05-29 17:47:13 +0200

    setup.py and LICENSE

--- setup.py
+++ setup.py
@@ -1,2 +1,16 @@
 from distutils.core import setup, Extension
-setup(name="homegear", version="1.0", ext_modules=[Extension("homegear", ["homegear.cpp", "IpcClient.cpp", "PythonVariableConverter.cpp"], extra_compile_args=['-std=c++11'], extra_link_args=['-lhomegear-ipc'])])
+setup(
+	name="homegear",
+	version="1.0",
+	description = 'Extension to connect to a local Homegear service.',
+	author="Homegear GmbH",
+	author_email="contact@homegear.email",
+	url="https://github.com/Homegear/libhomegear-python",
+	download_url = 'https://github.com/Homegear/libhomegear-python/archive/master.tar.gz',
+	keywords = ['homegear', 'smart home'],
+	ext_modules=[
+		Extension("homegear", ["homegear.cpp", "IpcClient.cpp", "PythonVariableConverter.cpp"],
+		extra_compile_args=['-std=c++11'],
+		extra_link_args=['-lhomegear-ipc'])
+	]
+)
