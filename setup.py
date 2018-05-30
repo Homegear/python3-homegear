0beb78f36ecd71864f0b58410721864c96212029
Sathya Laufer <sathya@laufers.net>
2018-05-30 16:02:19 +0200

    Small package fixes

--- setup.py
+++ setup.py
@@ -6,24 +6,23 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="homegear",
-	version="1.0.4",
+	version="1.0.5",
 	description = 'Extension to connect to a local Homegear service.',
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	author="Homegear GmbH",
 	author_email="contact@homegear.email",
 	url="https://github.com/Homegear/libhomegear-python",
-	download_url = 'https://github.com/Homegear/libhomegear-python/archive/1.0.4.tar.gz',
 	keywords = ['homegear', 'smart home'],
 	ext_modules=[
 		Extension("homegear", ["homegear.cpp", "IpcClient.cpp", "PythonVariableConverter.cpp"],
 		extra_compile_args=['-std=c++11'],
 		extra_link_args=['-lhomegear-ipc'])
 	],
-	package_data={'': ['IpcClient.h', "PythonVariableConverter.h"]},
+	include_package_data=True,
 	classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-        "Operating System :: POSIX"
-    )
+		"Programming Language :: C++",
+		"License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+		"Operating System :: POSIX"
+	)
 )
